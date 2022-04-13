9ad7f5268616eeee333f48af76274c28ba949162
thomas <t.g.abbink@gmail.com>
2022-04-13 14:30:37 +0200

    use scrolling in carousel

--- src/pages/carousel/Carousel.js
+++ src/pages/carousel/Carousel.js
@@ -1,33 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
+import SketchContainer from './SketchContainer'
 
 export default ({ children }) => {
-  const [currentSketch, setCurrentSketch] = useState(0)
-  const onNext = () => {
-    setCurrentSketch((currentSketch) => {
-      if (currentSketch === children.length - 1) {
-        return 0
-      }
-      return currentSketch + 1
-    })
-  }
-  const onPrev = () => {
-    setCurrentSketch((currentSketch) => {
-      if (currentSketch === 0) {
-        return children.length - 1
-      }
-      return currentSketch - 1
-    })
-  }
   return (
     <Container>
-      <Next onClick={onNext}>
-        <Chevron />
-      </Next>
-      <Prev onClick={onPrev}>
-        <LeftChevron />
-      </Prev>
-      <Card key={currentSketch}>{children[currentSketch]}</Card>
+      {children.map((s) => (
+        <SketchContainer sketch={s} key={s.key} />
+      ))}
     </Container>
   )
 }
@@ -36,49 +16,10 @@ const Container = styled.div`
   display: flex;
   flex-grow: 1;
   background-color: white;
-`
-const Card = styled.div`
-  display: flex;
-  flex-grow: 1;
-`
-
-const Flipper = styled.div`
-  display: flex;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  width: 3rem;
-  height: 100%;
-  align-items: center;
-  transition: 0.3s;
-
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.3);
-  }
-  &:active {
-    background-color: rgba(0, 0, 0, 0.4);
-  }
-`
-const Next = styled(Flipper)`
-  right: 0;
-  justify-content: flex-end;
-  padding-right: 5px;
-`
-
-const Prev = styled(Flipper)`
-  left: 0;
-  justify-content: flex-start;
-  padding-left: 5px;
-`
-
-const Chevron = styled.div`
-  width: 0;
-  height: 0;
-  border-top: 30px solid transparent;
-  border-bottom: 30px solid transparent;
-  border-left: 30px solid slategray;
-`
-
-const LeftChevron = styled(Chevron)`
-  transform: rotate(180deg);
+  scroll-behavior: smooth;
+  flex-direction: column;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  width: 100%;
+  scroll-snap-type: y mandatory;
 `
