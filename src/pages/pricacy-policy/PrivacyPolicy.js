5780000434689731e0570839337f2eec42cd99e3
Thomas Abbink <thomas.abbink@infi.nl>
2023-04-29 19:40:48 +0200

    renamed mac-mapper

--- src/pages/pricacy-policy/PrivacyPolicy.js
+++ src/pages/pricacy-policy/PrivacyPolicy.js
@@ -5,37 +5,37 @@ const PrivacyPolicy = () => {
   return (
     <div className="privacy-policy-container">
       <h1 className="privacy-policy-title">
-        Privacy Policy for mac-mapper Chrome Extension
+        Privacy Policy for Mac-Mapper Chrome Extension
       </h1>
       <p className="privacy-policy-text">
-        At mac-mapper, we take your privacy seriously. This Privacy Policy
+        At Mac-Mapper, we take your privacy seriously. This Privacy Policy
         explains how we handle the personal information that you provide to us
         or that we collect from you while using our Chrome Extension,
-        mac-mapper.
+        Mac-Mapper.
       </p>
       <h2 className="privacy-policy-subtitle">Information We Collect</h2>
       <p className="privacy-policy-text">
         We do not collect, transmit, or store any user data when you use our
-        mac-mapper Chrome Extension.
+        Mac-Mapper Chrome Extension.
       </p>
       <h2 className="privacy-policy-subtitle">Use of Cookies</h2>
       <p className="privacy-policy-text">
         We do not use cookies or any other similar technology to collect
-        information about you or your use of our mac-mapper Chrome Extension.
+        information about you or your use of our Mac-Mapper Chrome Extension.
       </p>
       <h2 className="privacy-policy-subtitle">
         Third-Party Websites and Services
       </h2>
       <p className="privacy-policy-text">
-        Our mac-mapper Chrome Extension may contain links to other websites or
-        services that are not owned or controlled by mac-mapper. We are not
+        Our Mac-Mapper Chrome Extension may contain links to other websites or
+        services that are not owned or controlled by Mac-Mapper. We are not
         responsible for the privacy practices of these third-party websites or
         services. We recommend that you review the privacy policies of these
         websites or services before using them.
       </p>
       <h2 className="privacy-policy-subtitle">Children’s Privacy</h2>
       <p className="privacy-policy-text">
-        Our mac-mapper Chrome Extension is not intended for children under the
+        Our Mac-Mapper Chrome Extension is not intended for children under the
         age of 13, and we do not knowingly collect personal information from
         children under the age of 13. If we become aware that we have collected
         personal information from a child under the age of 13, we will take
