334a5ad05c9509e2d1ace2d5345d8a4d85045721
Thomas Abbink <thomas.abbink@infi.nl>
2023-07-07 15:00:59 +0200

    made doodle responsive

--- src/sketches/generative/doodle.js
+++ src/sketches/generative/doodle.js
@@ -7,13 +7,15 @@ const doodle = (p5) => {
   let controlTop
   let controlBottom
   const lines = []
-
+  let w = 2
+  let h = 3
   p5.setup = () => {
     const { width, height } = getCanvasSize()
     p5.createCanvas(width, height)
-    const h = height - paddingHorizontal * 2
-    const controlDistanceFromPoint = h / 4
-
+    const availableHeight = height - paddingHorizontal * 2
+    const controlDistanceFromPoint = availableHeight / 4
+    w = width / 200
+    h = height / 400
     controlTop = p5.createVector(
       p5.width / 2,
       paddingHorizontal + controlDistanceFromPoint,
@@ -36,35 +38,53 @@ const doodle = (p5) => {
 
   p5.windowResized = () => {
     const { width, height } = getCanvasSize()
+    w = width / 100
+    h = height / 200
     p5.resizeCanvas(width, height)
   }
 
   const getTopControlVelocity = generateOscillatingNumber({
     initialValue: 0,
-    min: -10,
-    max: 10,
-    increment: 0.1,
+    min: -3,
+    max: 3,
+    increment: 0.05,
   })
-
-  const getBottomControlVelocity = generateOscillatingNumber({
+  const getTopControlYVelocity = generateOscillatingNumber({
     initialValue: 0,
-    min: -15,
-    max: 15,
-    increment: 0.2,
+    min: -2,
+    max: 2,
+    increment: 0.03,
+  })
+  const getBottomControlVelocity = generateOscillatingNumber({
+    initialValue: 1,
+    min: -1.5,
+    max: 1.5,
+    increment: 0.02,
+  })
+  const getBottomControlYVelocity = generateOscillatingNumber({
+    initialValue: 1,
+    min: -2,
+    max: 2,
+    increment: 0.03,
   })
   const backgroundOpacity = generateOscillatingNumber({
     min: 1,
     max: 20,
     initialValue: 1,
-    restFrames: 100,
+    restFrames: 300,
     increment: 0.1,
   })
+
   p5.draw = () => {
-    p5.background(0, 0, 0, backgroundOpacity())
     p5.noFill()
-    p5.stroke(255)
-    controlTop.x += getTopControlVelocity()
-    controlBottom.x += getBottomControlVelocity()
+    p5.background(0, 0, 0, backgroundOpacity())
+    p5.stroke(230, 230, 250, 200)
+
+    controlTop.x += getTopControlVelocity() * w
+    controlTop.y += getTopControlYVelocity() * h
+    controlBottom.x += getBottomControlVelocity() * w
+    controlBottom.y += getBottomControlYVelocity() * h
+
     p5.strokeWeight(3)
     lines.forEach((line) => {
       line.draw()
@@ -93,8 +113,12 @@ const line =
         isInverted ? p5.map(bottom.x, 0, p5.width, p5.width, 0) : bottom.x,
         bottom.y,
       )
+      // p5.push()
+      // p5.fill(255)
+      // p5.noStroke()
       // p5.ellipse(controlTop.x, controlTop.y, 10, 10)
       // p5.ellipse(controlBottom.x, controlBottom.y, 10, 10)
+      // p5.pop()
     }
 
     return { draw }
