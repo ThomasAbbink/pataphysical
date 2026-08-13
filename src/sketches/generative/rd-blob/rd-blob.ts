94e0ebb0d7f2aef1b551c953caaf5f447e46400a
ThomasAbbink <t.g.abbink@gmail.com>
2026-08-13 16:44:42 +0200

    update rd blob with e

--- src/sketches/generative/rd-blob/rd-blob.ts
+++ src/sketches/generative/rd-blob/rd-blob.ts
@@ -46,7 +46,7 @@ const rdBlob = (p5: p5js) => {
       size = height
     }
     if (!image) {
-      image = await p5.loadImage('/assets/walken.jpeg')
+      image = await p5.loadImage('/assets/e.png')
     }
     setupShaders()
     blitImage()
